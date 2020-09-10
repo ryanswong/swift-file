a3d9f2b718f6c17d705aada8f9d0eb13944471e0
Ryan Wong <ryan.suzuki.w@gmail.com>
2020-09-10 14:56:55 -0700

    added main swift file ui from qt designer

---
+++ test_uis/test_fileexpl.py
@@ -0,0 +1,21 @@
+from PySide2 import QtWidgets
+from PySide2.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+class Myview(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Myview, self).__init__()
+        # QtGui.QWidget.__init__(self)
+        model = QtWidgets.QFileSystemModel()
+        model.setRootPath('C:\Myfolder')
+        view = QtWidgets.QTreeView()
+        view.setModel(model)
+        self.setCentralWidget(view)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myview = Myview()
+    myview.show()
+    sys.exit(app.exec_())
